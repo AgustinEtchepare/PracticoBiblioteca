c35d2900c4e749d5aae13c76655a75d989f319f6
AgustinEtchepare <dagustinechpare@gmail.com>
2024-07-05 00:55:44 -0300

    Agregar archivos de proyecto.

---
+++ Biblioteca/Libro.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Libro.aspx.cs" Inherits="Biblioteca.Libro" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="ddlLibros" runat="server" AutoPostBack="true">
+                <asp:ListItem Selected="True" Value="autor1"> Autor1 </asp:ListItem>
+                <asp:ListItem Value="autor2"> Autor2 </asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        <div>
+            <gridview>
+
+            </gridview>
+        </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Biblioteca/Libro.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Biblioteca
+{
+    public partial class Libro : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            string connectionString = "Data Source=E01\\SQLEXPRESS;Initial Catalog=Biblioteca;Integrated Security=true";
+            string query = "select * from Libro";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                try
+                {
+                    conn.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    GridView1.DataSource = reader;
+                    GridView1.DataBind();
+                    
+                    reader.Close();
+                    conn.Close();
+
+                    
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+    }
+}
